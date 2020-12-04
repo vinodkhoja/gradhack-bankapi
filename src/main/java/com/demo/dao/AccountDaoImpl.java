a04e7e8de1156eb05b3be90d48646a0120cef0f7
vinodkhoja <73296945+vinodkhoja@users.noreply.github.com>
2020-12-04 16:44:41 +0530

    Update AccountDaoImpl.java

--- src/main/java/com/demo/dao/AccountDaoImpl.java
+++ src/main/java/com/demo/dao/AccountDaoImpl.java
@@ -1,3 +1,25 @@
+// package com.demo.dao;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.jdbc.core.JdbcTemplate;
+// import org.springframework.stereotype.Repository;
+
+// import com.demo.bean.Account;
+// @Repository
+// public class AccountDaoImpl implements AccountDao{
+// 	@Autowired 
+// 	private JdbcTemplate jdbcTemplate;
+
+// 	public String validateUser(Account account) {
+// 		String special_key = (String) jdbcTemplate.queryForObject("select special_key from hsbc_user where username = ? AND password_user = ? AND account_number = ?", new Object[] {account.getUsername(),account.getPasswordUser(),account.getAccountNumber()} , String.class);
+// 		if(special_key != null) {
+// 			return special_key;
+// 		}
+// 		else {
+// 			return "unreckonized user";
+// 		}
+// 	}
+// }
 package com.demo.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +31,11 @@ import com.demo.bean.Account;
 public class AccountDaoImpl implements AccountDao{
 	@Autowired 
 	private JdbcTemplate jdbcTemplate;
-
+	@Autowired 
+	ValidationDaoImpl validationDao = new ValidationDaoImpl(); 
 	public String validateUser(Account account) {
-		String special_key = (String) jdbcTemplate.queryForObject("select special_key from hsbc_user where username = ? AND password_user = ? AND account_number = ?", new Object[] {account.getUsername(),account.getPasswordUser(),account.getAccountNumber()} , String.class);
-		if(special_key != null) {
+		if(validationDao.validateUser(account) ==1) {
+			String special_key = (String) jdbcTemplate.queryForObject("select special_key from hsbc_user where username = ? AND password_user = ? AND account_number = ?", new Object[] {account.getUsername(),account.getPasswordUser(),account.getAccountNumber()} , String.class);
 			return special_key;
 		}
 		else {
